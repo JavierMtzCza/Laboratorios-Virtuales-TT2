5f057a6b2561edb261d7e4097d1a875523ea34da
Erick Diaz <erick.diazzamora@gmail.com>
2023-08-15 00:00:36 -0600

    prueba3

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,34 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import './cards.css'
+import PA6JuegoMemo from "../routes/PA6-JuegoMemo"
 
-const Cards = () => {
 
-    const images = [
-    "/memorama/imagen1.svg",
-    "/vite.svg",
-    
-    ]
-    const newImages = images.flatMap(item => [item, item])
-    console.log(newImages);
-    return (
-        
-        <div className="cards">
-         <h2> Score:100</h2>
-         <ul>
-            {
-                images.map((item, index) =>(
-                    <li key={index}>
-                        <div className="content">
-                            <div className="front">
-                                <img src={item} alt="" />
-                            </div>
-                        </div>
-                    </li>
-                ))
-            }
-         </ul>
-        </div>
-        
-       
-    );
-}
-
-export default Cards;
+ReactDOM.render(
+    <React.StrictMode>
+        <PA6JuegoMemo/>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
--- src/routes/PA6-JuegoMemo.jsx
+++ src/routes/PA6-JuegoMemo.jsx
@@ -1,20 +1,13 @@
-import { useState } from 'react'
-import Cards from '../components/Cards'
-import { Button, Divider, Form, Grid, Header, Icon, Image, Segment } from 'semantic-ui-react'
-import imagen from "../images/undraw_login_re_4vu2 1.svg"
-import { Link } from 'react-router-dom'
 
-const PA6JuegoMemo = () => {
+import './memorama.css'
+
+function PA6JuegoMemo() {
   return (
     
-    <div style={{
-        textAlign: "center" }}
-      className='PA6JuegoMemo'>
-
-        <h2>Memory Game</h2>
-        <Cards/>
+    <div className="App">
+        <h2>Magic Match</h2>
+        <button>Nuevo Juego</button>
     </div>
-    
   );
 }
 
