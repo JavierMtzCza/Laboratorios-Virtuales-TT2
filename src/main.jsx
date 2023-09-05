d6e29f25006fe6c89d9626ad93401e92b02c7f1f
Javier Martinez Carranza <Javier.martinez.carranzaipn@gmail.com>
2023-09-05 17:29:56 -0600

    assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { RouterProvider } from "react-router-dom"
 import './index.css'
 import 'semantic-ui-css/semantic.min.css'
